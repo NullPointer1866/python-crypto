60f9465858a851f4c746620ba5523c1b835ea8a7
Patrick Holmgren <pwh001@lvc.edu>
2017-11-15 19:08:06 -0500

    Update GenerateLSFRkey.py

--- GenerateLSFRkey.py
+++ GenerateLSFRkey.py
@@ -1,8 +1,13 @@
+'''
+Generates a key for LSFR sequence and applies it to a plaintext message.
+Can customize key generation by editing lines 10 and 29.
+Takes a "binary" string as input and outputs the same.
+'''
 def genkey(length, start):
     n = len(start)
 
     while n < length:
-        k = (start[n - 1] + start[n - 3] + start[n - 4]) % 2
+        k = (start[n - 1] + start[n - 3] + start[n - 4]) % 2 # Function to determine next bit
         start.append(k)
         n += 1
 
@@ -21,7 +26,7 @@ def buildCipher(plain, key):
     return cipher
 
 
-initial = [1, 0, 0, 1]
+initial = [1, 0, 0, 1] # Initial seeding for kit generation function
 plain = (input("Enter plaintext: "))
 plain = plain.replace(',', '')
 plain = plain.replace(' ', '')
